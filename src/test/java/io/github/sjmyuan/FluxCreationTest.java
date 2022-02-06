9cd676035fd385df4bda4d033be37969d2197618
sjmyuan <shang.jiaming@163.com>
2022-02-06 19:56:12 +0800

    add creation test

--- src/test/java/io/github/sjmyuan/FluxCreationTest.java
+++ src/test/java/io/github/sjmyuan/FluxCreationTest.java
@@ -1,7 +1,11 @@
 package io.github.sjmyuan;
 
+import java.util.HashSet;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.Set;
+import java.util.stream.Stream;
+
 import org.junit.Test;
 import reactor.core.publisher.Flux;
 import reactor.test.StepVerifier;
@@ -20,13 +24,20 @@ public class FluxCreationTest {
     }
 
     @Test
+    public void canBeCreatedFromASeriesOfString() {
+
+        StepVerifier.create(Flux.just("hello", "world", "!!")).expectNext("hello")
+                .expectNext("world").expectNext("!!").verifyComplete();
+    }
+
+    @Test
     public void canBeCreatedFromArray() {
-        StepVerifier.create(Flux.fromArray(new Integer[] {1, 2, 3, 4, 5})).expectNext(1)
+        StepVerifier.create(Flux.fromArray(new Integer[] { 1, 2, 3, 4, 5 })).expectNext(1)
                 .expectNext(2).expectNext(3).expectNext(4).expectNext(5).verifyComplete();
     }
 
     @Test
-    public void canBeCreatedFromIterable() {
+    public void canBeCreatedFromList() {
         List<Integer> list = new LinkedList<Integer>();
         list.add(1);
         list.add(2);
@@ -38,9 +49,38 @@ public class FluxCreationTest {
     }
 
     @Test
+    public void canBeCreatedFromSet() {
+        Set<Integer> set = new HashSet<Integer>();
+        set.add(1);
+        set.add(2);
+        set.add(2);
+        set.add(3);
+        set.add(4);
+
+        StepVerifier.create(Flux.fromIterable(set)).expectNext(1).expectNext(2).expectNext(3)
+                .expectNext(4).verifyComplete();
+    }
+
+    @Test
+    public void canBeCreatedFromStream() {
+        Stream<Integer> stream = Stream.of(1, 2, 3, 4, 5);
+        StepVerifier.create(Flux.fromStream(stream)).expectNext(1).expectNext(2).expectNext(3).expectNext(4)
+                .expectNext(5).verifyComplete();
+    }
+
+    @Test
     public void canBeCreatedFromThrowable() {
         StepVerifier.create(Flux.error(new Exception("some error")))
                 .verifyErrorMessage("some error");
     }
 
+    @Test
+    public void canGenerateARangeOfInteger() {
+        StepVerifier.create(Flux.range(1, 3)).expectNext(1).expectNext(2).expectNext(3).verifyComplete();
+    }
+
+    @Test
+    public void canGenerateEmpty() {
+        StepVerifier.create(Flux.empty()).verifyComplete();
+    }
 }
