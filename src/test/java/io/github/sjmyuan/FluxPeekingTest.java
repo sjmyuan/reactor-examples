e8593c80765ac4400e205a29b891940cd3873cda
sjmyuan <shang.jiaming@163.com>
2022-02-09 22:12:39 +0800

    add more peeking operation

--- src/test/java/io/github/sjmyuan/FluxPeekingTest.java
+++ src/test/java/io/github/sjmyuan/FluxPeekingTest.java
@@ -19,9 +19,64 @@ public class FluxPeekingTest {
     }
 
     @Test
-    public void canDoSideEffectForEveryElement() {
+    public void canDoSomethingForEveryElement() {
         List<Integer> list = new LinkedList<Integer>();
         flux.doOnNext(x -> list.add(x)).collectList().block();
         assertThat(list.size()).isEqualTo(6);
+    }
+
+    @Test
+    public void canMonitorTheStatusOfEachElement() {
+        List<Boolean> list = new LinkedList<Boolean>();
+        flux.doOnEach((signal) -> {
+            list.add(signal.isOnComplete());
+        }).collectList().block();
+        assertThat(list.size()).isEqualTo(7);
+        assertThat(list.get(0)).isFalse();
+        assertThat(list.get(1)).isFalse();
+        assertThat(list.get(2)).isFalse();
+        assertThat(list.get(3)).isFalse();
+        assertThat(list.get(4)).isFalse();
+        assertThat(list.get(5)).isFalse();
+        assertThat(list.get(6)).isTrue();
+    }
+
+
+    @Test
+    public void canDoSomethingWhenStreamComplete() {
+        List<Integer> list = new LinkedList<Integer>();
+        flux.doOnNext(x -> list.add(x)).doOnComplete(() -> list.add(8)).collectList().block();
+        assertThat(list.size()).isEqualTo(7);
+        assertThat(list.get(6)).isEqualTo(8);
+    }
+
+    @Test
+    public void canDoSomethingBeforeSequenceIsSubscribed() {
+        List<Integer> list = new LinkedList<Integer>();
+        flux.doOnNext(x -> list.add(x)).doFirst(() -> list.add(1)).doFirst(() -> list.add(2))
+                .doFirst(() -> list.add(3)).collectList().block();
+        assertThat(list.size()).isEqualTo(9);
+        assertThat(list.get(0)).isEqualTo(3);
+        assertThat(list.get(1)).isEqualTo(2);
+        assertThat(list.get(2)).isEqualTo(1);
+        assertThat(list.get(3)).isEqualTo(1);
+        assertThat(list.get(4)).isEqualTo(2);
+        assertThat(list.get(5)).isEqualTo(3);
+        assertThat(list.get(6)).isEqualTo(4);
+        assertThat(list.get(7)).isEqualTo(5);
+        assertThat(list.get(8)).isEqualTo(6);
+    }
+
+    @Test
+    public void canDoSomethingOnError() {
+
+        List<Integer> list = new LinkedList<Integer>();
+
+        Flux<Integer> flux2 = Flux.<Integer>error(new RuntimeException("error")).doOnError(
+                exception -> exception instanceof RuntimeException, exception -> list.add(1));
+
+        StepVerifier.create(flux2).verifyError();
+        assertThat(list.size()).isEqualTo(1);
+
     }
 }
